3d21f3252a8b1dec83751115fd09fe9330af065e
Jorge Gingsberg <jorgegingsberg@gmail.com>
2020-03-27 02:13:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(josh)
+  puts "Hi, my name is #{josh}."
 end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Dan, Ember.js)
+introduction_with_language("Dan","Ember.js")
