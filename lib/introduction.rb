2c04fde927821d5fad3a1eef77a3c60dab49e8d1
Jorge Gingsberg <jorgegingsberg@gmail.com>
2020-03-27 02:14:21 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(josh)
+  puts "Hi, my name is #{josh}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan","Ember.js")
